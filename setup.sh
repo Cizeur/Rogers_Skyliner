5d471070173105e31a50c7197309b387b24014f9
cizeur <cizeur@PrivateRogers>
2019-11-21 16:48:39 +0100

    crontab alternative

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 
-echo hello > /root/test
-./config.sh > /root/woooow
+croncmd="$SCRIPT_DIR/config.sh > /root/woooow"
+cronjob="@reboot $croncmd"
+( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+
