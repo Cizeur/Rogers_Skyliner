cd770cbe521a287d20ad266872fd23690c1177ce
cizeur <cizeur@PrivateRogers>
2019-11-21 15:27:01 +0100

    sudo takes an input

--- sudo.sh
+++ sudo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-login=$(eval getent passwd \
-	{$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} \
-	| cut -d: -f1| head -n 1)
+login=$1
 echo "Primary login"
 echo $login
 apt-get install sudo 
