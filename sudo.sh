7bd1c22770060a4782a45df666e103263abbc602
cizeur <cizeur@PrivateRogers>
2019-11-21 12:52:45 +0100

    sudoer done

--- sudo.sh
+++ sudo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-login=$(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} \
+
+login=$(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs).\
+	.$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} \
 	| cut -d: -f1| head -n 1)
+echo "Primary login"
 echo $login
+apt-get install sudo 
+usermod -aG $login
+
+
 
