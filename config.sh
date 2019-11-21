e044f25d0854fedd356d23e77eceb90e5b63da0a
cizeur <cizeur@PrivateRogers>
2019-11-21 14:46:36 +0100

    different interface reboot

--- config.sh
+++ config.sh
@@ -4,6 +4,10 @@
 #                      #
 ########################
 
+
+USER_BASIC=$(eval getent passwd \
+	{$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} \
+	| cut -d: -f1| head -n 1)
 STATIC_IP="10.13.254.77/30"
 GATEWAY="$(echo $STATIC_IP | cut -f1,2,3 -d'.').254"
 INTERFACE=$(ip addr show | awk '/inet.*brd/{print $NF; exit}')
@@ -21,3 +25,4 @@ sed -i "s#<GATEWAY>#$GATEWAY#g" ./REPLACEMENTS/interfaces
 mv /etc/network/interfaces /etc/network/interfaces.old
 cp ./REPLACEMENTS/interfaces /etc/network/interfaces
 sudo ifdown $INTERFACE && sudo ifup $INTERFACE
+print $USER_BASIC
