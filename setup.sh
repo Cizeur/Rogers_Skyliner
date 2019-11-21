c6664d943d2e93a59a458730c5f8d223901fc04d
cizeur <cizeur@PrivateRogers>
2019-11-21 16:54:45 +0100

    crontab alternative

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 
-crontab -e
+
+CRON_FILE="/var/spool/cron/root"
+if [ ! -f $CRON_FILE ]; then
+   echo "cron file for root doesnot exist, creating.."
+   touch $CRON_FILE
+   /usr/bin/crontab $CRON_FILE
+fi
 croncmd="$SCRIPT_DIR/config.sh > /root/woooow"
 cronjob="@reboot $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
