7b9856cae46367c18404566a1c52cd91afdecec1
cizeur <cizeur@PrivateRogers>
2019-11-21 13:43:48 +0100

    static ip done

--- config.sh
+++ config.sh
@@ -6,7 +6,18 @@
 
 STATIC_IP="10.13.254.77/30"
 GATEWAY="$(echo $STATIC_IP | cut -f1,2,3 -d'.').254"
-sed -i "s#dhcp#static#g" /etc/network/interfaces
-echo address $STATIC_IP >> /etc/network/interfaces
-echo gateway $GATEWAY   >> /etc/network/interfaces
+INTERFACE=$(ip addr show | awk '/inet.*brd/{print $NF; exit}')
+
+rm -rf REPLACEMENTS
+mkdir -p REPLACEMENTS
+
+#######################
+#    STATIC IP        #
+#######################
+
+sed -e "s#<INTERFACE>#$INTERFACE#g" ./templates/interface.TEMP > ./REPLACEMENTS/interface
+sed -i "s#<STATIC_IP>#$STATIC_IP#g" ./REPLACEMENTS/interface
+sed -i "s#<GATEWAY>#$GATEWAY#g" ./REPLACEMENTS/interface
+mv /etc/network/interface /etc/network/interface.old
+cp ./REPLACEMENTS/interface /etc/network/interface
 /etc/init.d/networking restart
