4122869fdc09dec0fe668ed83dbf7dcdb1928026
cizeur <cizeur@PrivateRogers>
2019-11-22 16:22:56 +0100

    packages

--- extra_packages.sh
+++ extra_packages.sh
@@ -1,6 +1,7 @@
 apt-get install -y  bmon
 debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
-debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Local only'"
 apt-get install -y postfix
+apt-get install -y alpine
 apt-get install -y fail2ban
 apt-get install -y ufw
