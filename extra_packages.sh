df38ca6ae88393e51adab84f5899668507a0e131
cizeur <cizeur@PrivateRogers>
2019-11-22 15:57:29 +0100

    added preconf postfix

--- extra_packages.sh
+++ extra_packages.sh
@@ -1,4 +1,6 @@
 apt-get install -y  bmon
+debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install -y postfix
 apt-get install -y fail2ban
 apt-get install -y ufw
