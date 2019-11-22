463c2b78db46b8a608eacf31993a2974aec42113
cizeur <cizeur@PrivateRogers>
2019-11-22 16:41:19 +0100

    nginx added

--- extra_packages.sh
+++ extra_packages.sh
@@ -3,5 +3,6 @@ debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Local only'"
 apt-get install -y postfix
 apt-get install -y alpine
+apt-get install -y nginx
 apt-get install -y fail2ban
 apt-get install -y ufw
