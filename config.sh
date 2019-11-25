1370f0e11b2f10758effd62008c242c3765c7947
cizeur <cizeur@PrivateRogers>
2019-11-25 12:11:10 +0100

    cron config

--- config.sh
+++ config.sh
@@ -143,6 +143,22 @@ fail2ban_set() {
 	systemctl restart fail2ban.service 
 }
 
+#######################
+#      CRONTAB        #
+#######################
+
+add_to_cron() {	
+	croncmd=$2
+	cronjob="$1 $croncmd"
+	( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+	systemctl restart cron.service
+}
+
+crontab_set(){
+	add_to_cron "0 4 * * 2" "/script/update.sh | tee -a  /var/log/update_script.log 2>&1"
+	add_to_cron "0 4 * * 5" "/script/manlog.sh"
+	add_to_cron "0 0 * * *" "/script/change_monitor.sh /etc/crontab /script/root"
+}
 
 #######################
 #       NGINX         #
