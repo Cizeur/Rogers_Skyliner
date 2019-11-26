00833a6692e06480968abfc1008f988586cc0015
Cizeur <cgiron@e3r5p7.42.fr>
2019-11-26 15:32:45 +0100

    check exec directory

--- config.sh
+++ config.sh
@@ -8,6 +8,8 @@ fi
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 
+echo $SCRIPT_DIR
+
 ########################
 #                      #
 #     VARIABLES        #
