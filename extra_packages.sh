ab8c432a70df40fff4c481d4cee4c2f1fdae2a8e
Cizeur <cgiron@e3r5p8.42.fr>
2019-11-27 10:49:01 +0100

    added non interactive

--- extra_packages.sh
+++ extra_packages.sh
@@ -1,6 +1,8 @@
 apt-get install -y  bmon
 debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Local only'"
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+sudo apt-get install -y -q
 apt-get install -y postfix
 apt-get install -y alpine
 apt-get install -y nginx
